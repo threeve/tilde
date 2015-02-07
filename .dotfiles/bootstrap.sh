b2eaff07f28d658b7bcc846c2fc8943143fba231
Jason Foreman <jason@threeve.org>
2015-02-07 01:05:36 -0600

    Add brew kegs and casks to install.

--- .dotfiles/bootstrap.sh
+++ .dotfiles/bootstrap.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
+set -e
 
 git config core.worktree ${HOME}
 
-./install-homebrew.sh
+[ -e /usr/local/bin/brew ] || ./install-homebrew.sh
+
+brew doctor
+
+taps=(
+    caskroom/fonts
+    )
+brew tap "${taps[@]}"
+
+brews=(
+    bash-completion
+    caskroom/cask/brew-cask
+    git
+    smartmontools
+    the_silver_searcher
+    )
+brew install "${brews[@]}"
+
+casks=(
+    1password
+    dropbox
+    google-chrome
+    macvim
+    moom
+    spotify
+    font-fira-sans
+    font-source-code-pro
+    )
+brew cask install "${casks[@]}"
